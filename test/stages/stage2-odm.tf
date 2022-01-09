fbf0c0786fb967c43c7c82643460137f13238b1e
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-09 05:52:53 +0530

    tls
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/stage2-odm.tf
+++ test/stages/stage2-odm.tf
@@ -7,6 +7,6 @@ module "gitops_module" {
   namespace = module.gitops_namespace.name
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
   cluster_type = module.dev_cluster.platform.type_code
-  //tls_secret_name = module.dev_cluster.platform.tls_secret
+  tls_secret_name = module.dev_cluster.platform.tls_secret
   //kubeseal_cert = module.argocd-bootstrap.sealed_secrets_cert
 }
