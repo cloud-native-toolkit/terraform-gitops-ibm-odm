db6c91a1de7486a72071e040e365e174d3412c1e
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-09 05:58:17 +0530

    removed kubeseal
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/stage2-odm.tf
+++ test/stages/stage2-odm.tf
@@ -8,5 +8,5 @@ module "gitops_module" {
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
   cluster_type = module.dev_cluster.platform.type_code
   //tls_secret_name = module.dev_cluster.platform.tls_secret
-  kubeseal_cert = module.argocd-bootstrap.sealed_secrets_cert
+  //kubeseal_cert = module.argocd-bootstrap.sealed_secrets_cert
 }
