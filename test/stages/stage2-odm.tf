b1fde28bae5e9d4bd41916ebc2643a5b82eb1b82
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-01-12 17:04:52 +0530

    updated stage-odm
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- test/stages/stage2-odm.tf
+++ test/stages/stage2-odm.tf
@@ -6,7 +6,5 @@ module "odm" {
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
-  cluster_type = module.dev_cluster.platform.type_code
-  //tls_secret_name = module.dev_cluster.platform.tls_secret
-  //kubeseal_cert = module.argocd-bootstrap.sealed_secrets_cert
+  cluster_type = module.dev_cluster.platform.type_code  
 }
